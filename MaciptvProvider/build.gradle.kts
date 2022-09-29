28dfffe56ab92c26dd295f9abeda7c059aa5311b
Eddy <kingkama976@gmail.com>
2022-09-29 21:07:53 +0200

    add stb mac IPTV

---
+++ MaciptvProvider/build.gradle.kts
@@ -0,0 +1,23 @@
+// use an integer for version numbers
+version = 1
+
+
+cloudstream {
+    language = "fr"
+    // All of these properties are optional, you can safely remove them
+
+    description = "IPTV avec Bein Sport RMC Sport ... Je vous laisse découvrir"
+    authors = listOf("Eddy")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf("Live")
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=franceiptv.fr/&sz=%size%"
+}
